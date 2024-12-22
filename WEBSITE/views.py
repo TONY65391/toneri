8bfd556d0865baba4fadeef2bdaf63ecd658abcd
TONY65391 <tonyanthony65391@gmail.com>
2024-12-22 14:15:55 -0800

    initial commit

---
+++ WEBSITE/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.template import loader
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404,HttpResponse
+
+# Create your views here.
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render(request = request))
