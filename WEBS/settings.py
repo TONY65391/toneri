28aebf3efb1939c73d5d4ec2a593f5d2b273d579
TONY65391 <tonyanthony65391@gmail.com>
2024-12-22 14:55:17 -0800

    2:55PM

--- WEBS/settings.py
+++ WEBS/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'WEBS.urls'
